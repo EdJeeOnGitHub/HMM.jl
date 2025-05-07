816b2e746d66a98761091f6c1935e554004bc558
Ed Jee <edjee96@gmail.com>
2025-05-07 14:07:33 -0500

    added stochsatic regression

---
+++ src/models/stochastic_regression.jl
@@ -0,0 +1,30 @@
+# --- Stochastic Regression HMM ---
+# Defines the data structures and algorithms for Stochastic EM with Robbins-Monro
+# updating for Regression Hidden Markov Models.
+
+# Assumes inclusion in the main HMM module scope.
+# Base types (AbstractHMMData) should be available from included base.jl
+
+"""
+    StochasticEMConfig
+
+Configuration structure for Stochastic EM with Robbins-Monro updating.
+
+# Fields
+- `τ::Int`: Robbins-Monro updating parameter
+- `t::Int`: Current iteration count for learning rate scheduling
+- `batch_size::Int`: Size of mini-batch to use for stochastic updates
+"""
+mutable struct StochasticEMConfig
+    τ::Int
+    t::Int
+    full_batch_step::Int
+    batch_size::Int
+end
+function StochasticEMConfig(; τ=100, t=0, full_batch_step=10, batch_size=20)
+    return StochasticEMConfig(τ, t, full_batch_step, batch_size)
+end
+
+
+
+
